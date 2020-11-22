cc6dc913cf44b832c88a008f178d79d345297150
EEAjaegbu <74838017+EEAjaegbu@users.noreply.github.com>
2020-11-22 16:57:46 +0100

    Update projectwebapp.py

--- projectwebapp.py
+++ projectwebapp.py
@@ -7,7 +7,7 @@
 ## Load libaries
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 from PIL import Image
